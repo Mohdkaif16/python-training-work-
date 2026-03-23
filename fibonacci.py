dd1efc7c6896d3fdf5b6ccd4474b8cd0f6a60b48
Mohdkaif16 <kaifm7236@gmail.com>
2026-03-23 09:52:44 +0530

    Create fibonacci.py

---
+++ fibonacci.py
@@ -0,0 +1,20 @@
+# using recursion function
+def fib(n):
+    if n<=1:
+        return n
+    return fib(n-1)+fib(n-2)
+n=int(input("enter the number"))
+print(fib(n))
+# without recursion function
+def fib(n):
+    if n <= 1:
+        return n
+    a = 0 
+    b = 1   
+    for i in range(2, n + 1):
+        c = a + b
+        a = b
+        b = c
+    return b
+n = int(input("Enter n: "))
+print(fib(n))
