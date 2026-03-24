b76f6bfa1fc9b60c28d5d8ea5eb1893933f3817d
Mohdkaif16 <kaifm7236@gmail.com>
2026-03-24 10:43:41 +0530

    Update fibonacci(509).py

--- fibonacci(509).py
+++ fibonacci(509).py
@@ -19,3 +19,32 @@ def fib(n):
     return b
 n = int(input("Enter n: "))
 print(fib(n))
+# using dynamic programming 
+class Solution:
+    def fib(self, n: int) -> int:
+        dp = [-1 for i in range(n + 1)]   # moved up (only placement fix)
+
+        def fib(n, dp):                  # wrapped your same function
+            if n == 0 or n == 1:
+                return n
+
+            if dp[n] != -1:
+                return dp[n]
+
+            if dp[n - 1] == -1:
+                ans1 = fib(n - 1, dp)
+                dp[n - 1] = ans1
+            else:
+                ans1 = dp[n - 1]
+
+            if dp[n - 2] == -1:
+                ans2 = fib(n - 2, dp)
+                dp[n - 2] = ans2
+            else:
+                ans2 = dp[n - 2]
+
+            dp[n] = ans1 + ans2
+            return dp[n]
+
+        ans = fib(n, dp)
+        return ans
