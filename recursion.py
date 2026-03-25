4c3fd12a663ebea47156839a4292f71b5eee5b7d
Mohdkaif16 <kaifm7236@gmail.com>
2026-03-25 10:03:26 +0530

    Update recursion.py

--- recursion.py
+++ recursion.py
@@ -17,13 +17,22 @@ n =int(input("enter the number\n"))
 print_reverse(n)
 #sum of all element of the array recursivly
 def sum(l):
-    if len(l) == 1:
-        return l[0]
-    return l[0]+sum(l[1:])
-l=[]    
+    if len(l) == 1:                 #if len(l) == 1:
+         return l[0]                    #return l[0] 
+    return l[0]+sum(l[1:])          #small=sum(l[1:])
+l=[]                                #return l[0]+small
 n =int(input("enter the number of items \n"))
 for i in range(n):
     x=int(input("enter the number"))
     l.append(x)
 print("list",l)    
 print("sum",sum(l))
+#find factorial recursivly 
+def fact(n):
+    if n==1 or n==0:
+        n=1
+        return n 
+    s=fact(n-1)
+    return s*n
+n=int(input("enter the number"))
+print(fact(n))
