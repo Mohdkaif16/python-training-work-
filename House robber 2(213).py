45658d85e7d699382e48fbfc64e7add6af8ab8a8
Mohdkaif16 <kaifm7236@gmail.com>
2026-03-28 11:56:35 +0530

    Update House robber 2(213).py

--- House robber 2(213).py
+++ House robber 2(213).py
@@ -13,3 +13,27 @@ class Solution:
         return max(
             helper(nums[:-1]),
             helper(nums[1:]))
+# user define 
+def rob(nums):
+    def helper(arr):
+        prev1, prev2 = 0, 0
+        for num in arr:
+            curr = max(prev1, prev2 + num)
+            prev2 = prev1
+            prev1 = curr
+        return prev1
+
+    n = len(nums)
+
+    if n == 1:
+        return nums[0]
+
+    return max(
+        helper(nums[:-1]),
+        helper(nums[1:]))
+l=[]        
+x=int(input("enter the number of items"))
+for i in range(x):
+    y=int(input("enter the number"))
+    l.append(y)
+print(rob(l))
