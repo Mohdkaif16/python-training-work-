ef8bed17519d9959cd562acc1b7e0db7d5614955
Mohdkaif16 <kaifm7236@gmail.com>
2026-04-02 10:08:56 +0530

    Update perfect square(279).py

--- perfect square(279).py
+++ perfect square(279).py
@@ -1,6 +1,7 @@
 class Solution:
     def numSquares(self, n: int) -> int:
-        dp = [0] + [float('inf')] * n
+        dp = [0] + [float('inf')] * n #float("inf") where inf is infinity . In DP, we want to find the minimum value So we start with a very large number so that any real answer becomes smalle.
+                                      #We don’t know the answer → set it to infinity as defulte .
         for i in range(1, n + 1):
             j = 1
             while j*j <= i:
