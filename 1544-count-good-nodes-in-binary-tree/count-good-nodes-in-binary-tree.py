40f55dfc77d8e39f21e761c8e60d9e53454120ca
Mohdkaif16 <kaifm7236@gmail.com>
2026-04-15 09:58:12 +0530

    Time: 128 ms (63.08%) | Memory: 32 MB (44.57%) - LeetSync

--- 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
+++ 1544-count-good-nodes-in-binary-tree/count-good-nodes-in-binary-tree.py
@@ -10,16 +10,11 @@ class Solution:
         def dfs(node, max_val):
             if node is None:
                 return 0
-
             count = 0
-
             if node.val >= max_val:
                 count = 1
                 max_val = node.val
-
             count += dfs(node.left, max_val)
             count += dfs(node.right, max_val)
-
             return count
-
         return dfs(root, root.val) 
