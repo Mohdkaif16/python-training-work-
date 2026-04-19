cba1f5a39972b6b68b76ad3904d0114b6cd7929a
Mohdkaif16 <kaifm7236@gmail.com>
2026-04-19 18:12:19 +0530

    Time: 0 ms (100.00%) | Memory: 19.4 MB (87.14%) - LeetSync

---
+++ 225-implement-stack-using-queues/implement-stack-using-queues.py
@@ -0,0 +1,20 @@
+class MyStack:
+
+    def __init__(self):
+        self.q = []
+
+    def push(self, x):
+        self.q.append(x)
+        
+        # rotate queue
+        for _ in range(len(self.q) - 1):
+            self.q.append(self.q.pop(0))
+
+    def pop(self):
+        return self.q.pop(0)
+
+    def top(self):
+        return self.q[0]
+
+    def empty(self):
+        return len(self.q) == 0
